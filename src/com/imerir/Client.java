acc194b1ac5b667b269be77b6e7aa8c054ef81ff
Corentin LEQUEUX <corentin.lequeux@imerir.com>
2021-06-14 13:05:20 +0000

    Update Client.java

--- src/com/imerir/Client.java
+++ src/com/imerir/Client.java
@@ -1,10 +1,32 @@
 package com.imerir;
 
+import java.util.Random;
+
 public class Client {
 
-    public void createClient(Weather weather) {
+    private int nbClient;
+    private Lemonade lemonade;
+    private float ratioClient;
+    private float ratioReel;
 
-        System.out.println(weather.getPourcentageMax());
+    public void createClient(Weather weather, Lemonade lemonade) {
 
+        Random random = new Random();
+        double randNum = weather.getPourcentageMin() + (weather.getPourcentageMax() - weather.getPourcentageMin()) * random.nextDouble();
+        Double client = new Double(randNum * 40);
+
+        nbClient = client.intValue();
+        ratioClient = nbClient / 5;
+        if (ratioClient > lemonade.getNumAds()) {
+            ratioReel = lemonade.getNumAds() / ratioClient;
+            nbClient *= ratioReel;
+            if(nbClient > lemonade.getNumLemonade()) {
+                nbClient = lemonade.getNumLemonade();
+            }
+        }
+    }
+
+    public int getNbClient() {
+        return nbClient;
     }
 }
