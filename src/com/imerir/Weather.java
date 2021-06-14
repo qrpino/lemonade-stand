dc7180eba4ab23dc13ca6f18911d775e7ba0830b
Corentin <corentin.lequeux@imerir.com>
2021-06-14 11:50:20 +0200

    First Upload

--- src/com/imerir/Weather.java
+++ src/com/imerir/Weather.java
@@ -4,14 +4,13 @@ public enum Weather {
 
     HOT(40, 90), SUNNY(25, 60), CLOUDY(5, 20);
 
-    private int[] pourcentage;
+    private int [] pourcentage;
 
     private Weather(int min, int max) {
-        pourcentage[0] = min;
-        pourcentage[1]= max;
+        int [] pourcentage = {min, max};
     }
 
-    public int[] getValue() {
+    public int[] getPourcentage() {
         return pourcentage;
 
     }
