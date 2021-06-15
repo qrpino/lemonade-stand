29c687c5812185b3facfb75239af73d05e641896
Vini PRAION <vini.praion@imerir.com>
2021-06-15 13:45:22 +0200

    test

--- src/com/imerir/Main.java
+++ src/com/imerir/Main.java
@@ -3,6 +3,7 @@ package com.imerir;
 public class Main {
 
     public static void main(String[] args) {
+        // Commit test
         MainWindow window = new MainWindow();
         window.setVisible(true);
         Game game = new Game();
